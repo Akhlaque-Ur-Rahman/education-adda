6c6cc53ec0ca55d2b5ae725a8598117d8017b283
Akhlaque-Ur-Rahman <ikhlakhrehman@gmail.com>
2025-09-03 12:57:58 +0530

    Why Choose Us Adjusted according to design

--- components/sections/whyChooseUs.tsx
+++ components/sections/whyChooseUs.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 const WhyChooseUs = () => {
   return (
-    <section className="p-[60px] bg-[#F5EDED] w-full flex justify-center items-center">
-      <div className="cta-container flex flex-col lg:flex-row items-center xl:justify-center  h-auto lg:h-[380px] relative lg:px-[60px] max-w-[2500px]">
+    <section className="p-[60px] bg-[#F5EDED] w-full flex  items-center">
+      <div className="cta-container flex flex-col lg:flex-row lg:items-center  w-full lg:h-[380px] relative 2xl:px-[60px] ">
         <img
           src="/svg/triangle-right.svg"
           alt=""
@@ -17,36 +17,44 @@ const WhyChooseUs = () => {
         />
 
         {/* Left Card */}
-        <div className="left-card bg-[#FF0000] w-full xl:w-fit xl:gap-[20px] p-6 text-white lg:rounded-tr-[40px] lg:rounded-bl-[40px] h-full flex flex-col justify-center xl:py-[32px] xl:px-[16px]">
+        <div
+          className="left-card bg-[#FF0000] text-white flex flex-col justify-center p-6 lg:rounded-tr-[40px] lg:rounded-bl-[40px] xl:flex-shrink-0 h-full w-full lg:max-w-[550px]"
+          
+        >
           <h2
-            className=""
-            style={{ fontSize: 'clamp(24px, 5vw, 36px)' }}
+            className="leading-snug"
+            style={{ fontSize: "clamp(24px, 5vw, 36px)" }}
           >
-            <span className='font-bold'>500+</span> Hours of Live & Recorded Classes
-            
-            Learn with <span className='text-[#D8FDB4]'></span>Top mentors of 
-            <span className="font-bold"> Education Adda</span>
+            <span className="font-bold">500+</span> Hours of Live & Recorded
+            Classes
+            <br />
+            Learn with <span className="text-[#D8FDB4]">Top mentors</span> of{" "}
+            <span className="font-bold">Education Adda</span>
           </h2>
-          <div className="btn-container w-full flex items-center border-t-2 border-white ">
-            <button className="rounded-[15px] px-4 xl:px-[30px] py-2.5 xl:py-[20px] text-[#FF0000] bg-white font-semibold mt-9">
+
+          <div className="btn-container border-t-2 border-white mt-6 pt-6">
+            <button className="rounded-[15px] px-6 py-3 bg-white text-[#FF0000] font-semibold">
               Explore Courses
             </button>
           </div>
         </div>
 
         {/* Right Image */}
-        <div className="photo-container relative w-full h-[250px] sm:h-[300px] md:h-[400px]  lg:h-full border-[#ff0000] border-[10px] flex justify-center">
-          <Image
-            src="/images/scenary.jpg"
-            alt="whyChooseUs"
-            fill
-            sizes="(max-width: 768px) 100vw, 
+        <div className="photo-container relative w-full lg:flex-1 lg:w-[400px] 
+     h-[250px] sm:h-[300px] md:h-[380px] lg:h-[380px] xl:h-[380px]
+     border-[#ff0000] border-[10px] flex justify-center ">
+  <Image
+    src="/images/fauxels.jpg"
+    alt="whyChooseUs"
+    fill
+    sizes="(max-width: 768px) 100vw, 
             (max-width: 1200px) 50vw, 
             50vw"
-            className="object-cover bg-center "
-            priority
-          />
-        </div>
+    className="object-cover object-center"
+    priority
+  />
+</div>
+
       </div>
     </section>
   )
